138932b771bccf8e4a1b52f86f0dd2ce3cb8875e
Brandon Burgess <burgess.brandon@student.greenriver.edu>
2023-01-05 17:41:55 -0500

    added php to process

--- index.php
+++ index.php
@@ -10,8 +10,13 @@
 <body>
     <h1>Pick Yo Cupcake</h1>
 
+
+
     <form>
-        <input type="checkbox" value="The Grasshopper" name="grass">The Grasshopper</input><br>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br><br>
+
+        <input type="checkbox" value="The Grasshopper" name="grass[]">The Grasshopper</input><br>
         <input type="checkbox" value="Whiskey Maple Bacon" name="whisk">Whiskey Maple Bacon</input><br>
         <input type="checkbox" value="Carrot Walnut" name="carwal">Carrot Walnut</input><br>
         <input type="checkbox" value="Salted Caramel" name="sarcar">Salted Caramel</input><br>
